b706c71213ea86c8d2e10928241972191a8b27a0
ryan <ryan@outs.in>
2013-03-02 06:40:46 +1100

    clean.sh v2

--- clean.sh
+++ clean.sh
@@ -1,1 +1,5 @@
 find . -type f -name "*.so" -o -name "*.pyc" -o -name "*.pyd" | xargs rm -f
+rm -f pyspades/{bytes,common,contained,loaders,mapmaker,packet,vxl,world}.cpp
+rm -rf build
+rm -rf enet/build
+rm -f enet/enet.c
